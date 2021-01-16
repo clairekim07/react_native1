8479478e751d8a0a8d843e045c9cfc9302c43007
clairekim07 <clairesekim@gmail.com>
2021-01-15 19:27:50 -0800

    answered the questions

---
+++ App.js
@@ -0,0 +1,45 @@
+import React, {Component} from 'react'
+import{View,Text,Spreadsheet} from 'react-native'
+
+export default class Test extends Component{
+  render(){
+    return <Text>1. Which one of the following language is an Imperative Language?                                               HTML, CSS, or Java Script.   
+    Answer: 	Java Script    2. Which one of the following is a Declarative Language?
+
+HTML,CSS, or Java Script.         
+
+Answer:HTML and CSS.                                              3. Name two uses of a DIV tag?
+
+Answer:Distinguishing or classifying from other pieces of code and to break up the code into smaller pieces to make it easier to read.                  4. What is the difference between relative positioning and absolute positioning in HTML?
+Answer: Relative positioning is when only the object moves. Absolute positioning is when the surroundings can move too like the layout.             5. What is the use of opacity in CSS?                Answer:It is to use when you want something to have less opacity when you hover over it or for any other reason.                                                               6.        
+Which is the programming language used in the React Native Framework?
+
+Answer: JSX (a different type of javascript).                                                       7. 
+Which online editor are we using for creating our apps in React Native Framework?
+
+Answer:facebook
+
+8. Write the steps to test your first designed app in the online editor on a mobile.
+
+Answer:You first import the react libraries. Then, you make a child class from Component and add render and return. You put mostly text and view so you can
+see what you can type on your web or the style of it.              9. 
+What is the use of the render function in React Native Framework??
+
+Answer: It is for sharing code between the react component using a prop.         10. 
+What is the use of return function  in React Native Framework??
+
+Answer:It is for displaying what you put in it. Anything you want to display. Like if you want to show a text saying press me, you would have to do that in the return function.       11. 
+What are the various components in your first app that you designed??
+
+Answer: Some components that I used were View and Text.
+
+
+
+
+
+
+
+</Text>
+
+  }
+}
